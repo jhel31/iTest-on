1817c7afef0fabc621bda34b980d752be09eb23b
jhel31 <adrianoesq11@gmail.com>
2021-09-15 18:09:09 -0500

    Subiendo jpaRepository
    para que no tengan que crearlo cada uno

--- src/main/java/pe/edu/upc/iTeston/models/repository/CareerRepository.java
+++ src/main/java/pe/edu/upc/iTeston/models/repository/CareerRepository.java
@@ -1,5 +1,9 @@
 package pe.edu.upc.iTeston.models.repository;
 
-public interface CareerRepository {
+import java.util.List;
 
+import pe.edu.upc.iTeston.models.entities.Career;
+
+public interface CareerRepository extends JpaRepository<Career, String> {
+	List<Career> name_career(String name) throws Exception;
 }
---
+++ src/main/java/pe/edu/upc/iTeston/models/repository/QuizRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.iTeston.models.repository;
+
+import pe.edu.upc.iTeston.models.entities.Quiz;
+
+public interface QuizRepository extends JpaRepository<Quiz, String> {
+
+}
