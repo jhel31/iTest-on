505d04e17508453775528987b6e597f08dda40f9
jhel31 <adrianoesq11@gmail.com>
2021-09-15 01:55:22 -0500

    Meti los packgage
    ya solo tienen que agregar las clases que tienen que implementar cada uno

---
+++ src/main/java/pe/edu/upc/iTeston/business/crud/CareerService.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.iTeston.business.crud;
+
+public interface CareerService {
+
+}
---
+++ src/main/java/pe/edu/upc/iTeston/models/repository/CareerRepository.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.iTeston.models.repository;
+
+public interface CareerRepository {
+
+}
