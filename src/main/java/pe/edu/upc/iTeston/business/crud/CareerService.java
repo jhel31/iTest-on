c2fe9e2525a17965987e6f7571a35a3196c8ab04
jhel31 <adrianoesq11@gmail.com>
2021-09-15 21:48:20 -0500

    Serrvices completos

--- src/main/java/pe/edu/upc/iTeston/business/crud/CareerService.java
+++ src/main/java/pe/edu/upc/iTeston/business/crud/CareerService.java
@@ -7,4 +7,5 @@ import pe.edu.upc.iTeston.models.entities.Career;
 public interface CareerService extends CrudService<Career, String> {
 	
 	List<Career> name_career(String name) throws Exception;
+	
 }
