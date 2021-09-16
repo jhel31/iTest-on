6be158366061afd4e3dcf1f5e842293f826a5ce2
jhel31 <adrianoesq11@gmail.com>
2021-09-15 21:41:21 -0500

    mejoras
    ga

--- src/main/java/pe/edu/upc/iTeston/business/crud/CareerService.java
+++ src/main/java/pe/edu/upc/iTeston/business/crud/CareerService.java
@@ -1,7 +1,10 @@
 package pe.edu.upc.iTeston.business.crud;
 
+import java.util.List;
+
 import pe.edu.upc.iTeston.models.entities.Career;
 
 public interface CareerService extends CrudService<Career, String> {
-
+	
+	List<Career> name_career(String name) throws Exception;
 }
