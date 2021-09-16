da324b52bb9654390fd94df8701d9069581ac262
AnthonR1 <rojasespichananthony26@gmail.com>
2021-09-15 21:58:14 -0500

    Update PaymentMethod
    entidad

---
+++ src/main/java/pe/edu/upc/iTeston/models/entities/PaymentMethod.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.iTeston.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity 
+@Table(name = "PaymentMethods")
+
+
+
+public class PaymentMethod {
+
+}
---
+++ src/main/java/pe/edu/upc/iTeston/models/entities/Student.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.iTeston.models.entities;
+
+public class Student {
+
+}
