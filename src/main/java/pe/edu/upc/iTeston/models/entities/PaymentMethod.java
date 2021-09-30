acd8f00373d6bcd513f4a6c88f835026461022c8
AnthonR1 <rojasespichananthony26@gmail.com>
2021-09-30 09:25:55 -0500

    TP
    ANTHONY

--- src/main/java/pe/edu/upc/iTeston/models/entities/PaymentMethod.java
+++ src/main/java/pe/edu/upc/iTeston/models/entities/PaymentMethod.java
@@ -28,13 +28,50 @@ public class PaymentMethod {
 	@Transient
 	private int codeCard;
 	
-	@Column(name = "pay_Subcription", nullable = false)
-	private float pay;
-	
 	@Column(name = "expire_date")
 	@Temporal(TemporalType.DATE)
 	private Date date;
 	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getNumberCard() {
+		return numberCard;
+	}
+
+	public void setNumberCard(int numberCard) {
+		this.numberCard = numberCard;
+	}
+
+	public int getCodeCard() {
+		return codeCard;
+	}
+
+	public void setCodeCard(int codeCard) {
+		this.codeCard = codeCard;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public List<Subscription> getSubscriptions() {
+		return subscriptions;
+	}
+
+	public void setSubscriptions(List<Subscription> subscriptions) {
+		this.subscriptions = subscriptions;
+	}
+
 	@OneToMany(mappedBy="paymentMethod", fetch = FetchType.LAZY)
 	private List<Subscription> subscriptions;
 
--- src/main/java/pe/edu/upc/iTeston/models/entities/Subscription.java
+++ src/main/java/pe/edu/upc/iTeston/models/entities/Subscription.java
@@ -18,12 +18,68 @@ public class Subscription {
 	@Column(name = "id_subscription", columnDefinition="NUMERIC(4)")
 	private String id;
 	
-	@Column(name = "issueDate_subcription", length = 9, nullable = false)
+	@Column(name = "issueDate_subcription", length = 50, nullable = false)
 	private Date issueDate;
 	
-	@Column(name = "expire_subcription", length = 9, nullable = false)
+	@Column(name = "expire_subcription", length = 50, nullable = false)
 	private String expire;
 	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(Date issueDate) {
+		this.issueDate = issueDate;
+	}
+
+	public String getExpire() {
+		return expire;
+	}
+
+	public void setExpire(String expire) {
+		this.expire = expire;
+	}
+
+	public PaymentMethod getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(PaymentMethod paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public List<Quiz> getQuizzes() {
+		return quizzes;
+	}
+
+	public void setQuizzes(List<Quiz> quizzes) {
+		this.quizzes = quizzes;
+	}
+
+	public float getPlan() {
+		return plan;
+	}
+
+	public void setPlan(float plan) {
+		this.plan = plan;
+	}
+
 	@ManyToOne
 	@JoinColumn(name="id_paymentMethods")
 	private PaymentMethod paymentMethod;
