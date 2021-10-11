be23f0652b09411d6960102e8e632bf3f300da73
Marcel-19 <renzoesp17@gmail.com>
2021-10-11 10:52:40 -0500

    CommentView completed
    CommentView completed, corrections in the service, repository and Java

--- src/main/java/pe/edu/upc/iTeston/models/repository/impl/CommentRepositoryImpl.java
+++ src/main/java/pe/edu/upc/iTeston/models/repository/impl/CommentRepositoryImpl.java
@@ -24,8 +24,14 @@ public class CommentRepositoryImpl implements CommentRepository{
 	}
 
 	@Override
-	public Optional<Comment> findById(Integer id) throws Exception {
+	public Optional<Comment> findById(String id) throws Exception {				
 		return findById(id, Comment.class);
+	}
+	@Override
+	public List<Comment> findByDescription(String description) throws Exception {
+		String jpql = "SELECT c FROM Comment c WHERE c.description_comment LIKE '%" + description + "%'" ;
+		System.out.println(jpql);
+		return findAll(Comment.class, jpql);
 	}
 
 	@Override
